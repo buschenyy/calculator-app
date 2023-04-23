66281d8f8b2dcc5af3b6bbe62953fe706db924b8
buschenyy <buschenyy@gmail.com>
2023-04-23 15:28:28 +0200

    intput logic and calculation

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,86 @@
+import { useState } from 'react'
 import './App.css'
 import Button from './components/Button'
 const operators = ['.', '/', 'x', '-', '+', 'del', 'reset', '=']
 const buttons = Array.from({ length: 10 }, (_, i) => i).concat(operators)
+const calcMemoInitial = {
+  num1: '',
+  operator: '',
+  num2: '',
+}
+
+const calculation = ({ num1, num2, operator }) => {
+  const a = parseInt(num1),
+    b = parseInt(num2)
+  switch (operator) {
+    case '/':
+      return a / b
+    case 'x':
+      return a * b
+    case '-':
+      return a - b
+    case '+':
+      return a + b
+  }
+}
 
 function App() {
+  const [calcMemo, setCalcMemo] = useState(calcMemoInitial)
+  const [output, setOutput] = useState('')
+
+  const buttonHandler = (target) => {
+    if (typeof target === 'number') {
+      if (calcMemo.operator && !calcMemo.num2) {
+        setOutput(target.toString())
+      } else {
+        setOutput(output.concat(target))
+      }
+      setCalcMemo({
+        ...calcMemo,
+        [calcMemo.operator ? 'num2' : 'num1']: calcMemo.operator
+          ? calcMemo.num2.concat(target)
+          : calcMemo.num1.concat(target),
+      })
+    } else {
+      switch (target) {
+        case 'reset':
+          setCalcMemo(calcMemoInitial)
+          setOutput('')
+          break
+        case 'del':
+          setOutput(output.slice(0, -1))
+          setCalcMemo({
+            ...calcMemo,
+            [calcMemo.operator ? 'num2' : 'num1']: output.slice(0, -1),
+          })
+          break
+        case '=':
+          setOutput(calculation(calcMemo).toString())
+          setCalcMemo({
+            ...calcMemoInitial,
+            num1: calculation(calcMemo).toString(),
+          })
+          break
+        default:
+          setCalcMemo({ ...calcMemo, operator: target })
+      }
+    }
+  }
   return (
     <div>
       <div>
         <span>calc</span>
         <span>theme</span>
       </div>
-      <div>Output</div>
-      <div>calculator</div>
+      <div>{output}</div>
       <div className="operationPad">
-        {buttons.map((num, i) => (
-          <Button key={`btn${num}`} num={num} i={i} />
+        {buttons.map((digit, i) => (
+          <Button
+            key={`btn${i}`}
+            onClick={() => buttonHandler(digit)}
+            digit={digit}
+            i={i}
+          />
         ))}
       </div>
     </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,9 @@
-const Button = ({ num, i }) => {
-  return <button className={`btn${i + 1}`}>{num}</button>
+const Button = ({ digit, i, onClick }) => {
+  return (
+    <button className={`btn${i + 1}`} onClick={onClick}>
+      {digit}
+    </button>
+  )
 }
 
 export default Button
