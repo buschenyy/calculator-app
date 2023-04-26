b821fe166691593c8f2e054135b63c874b0e3928
buschenyy <buschenyy@gmail.com>
2023-04-26 23:12:09 +0200

    +styles for buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,15 @@
+import cn from 'classnames/bind'
+
 const Button = ({ value, i, theme, onClick }) => {
   const valueForDisplay = value !== 'result' ? value : '='
+  const classNames = {
+    button: value !== 'reset' && value !== 'del' && value !== 'result',
+    darkButton: value === 'reset' || value === 'del',
+    brightButton: value === 'result',
+  }
+
   return (
-    <button className={`btn${i + 1} ${theme} buttonDark`} onClick={onClick}>
+    <button className={cn(`btn${i + 1}`, theme, classNames)} onClick={onClick}>
       {valueForDisplay}
     </button>
   )
