9ee2b365a6fd1c1f80ace2ba6b63ed73785e5ad9
buschenyy <buschenyy@gmail.com>
2023-04-30 17:09:24 +0200

    changes: state consists of string + updateValue handling

--- src/App.jsx
+++ src/App.jsx
@@ -6,56 +6,34 @@ import { operationButtons } from './data/operButtons'
 import { reducer } from './utils/reducer'
 import ThemeSwitch from './components/ThemeSwitch'
 const calcMemoInit = {
-  operand1: null,
-  operator: null,
-  operand2: null,
-  floatBuffer: '',
+  operand1: '',
+  operator: '',
+  operand2: '',
   calculated: false,
 }
 
 function App() {
   const [theme, setTheme] = useState('darkBlue')
   const [calcState, dispatch] = useReducer(reducer, calcMemoInit)
-  const currentOperand =
-    calcState.operator && calcState.operand2 !== null ? 'operand2' : 'operand1'
+  const currentOperand = calcState.operator ? 'operand2' : 'operand1'
 
-  let displayValue = calcState[currentOperand]
+  const onClickHandler = (action) => {
+    dispatch(action)
+  }
 
-  // If there is a float buffer, display it
-  if (calcState.floatBuffer) {
-    displayValue = calcState.floatBuffer
-  }
-  // If the value is a decimal and the calculation has been completed, round it to 9 decimal places
-  else if (
-    !Number.isInteger(displayValue) &&
-    calcState.calculated &&
-    displayValue?.toString().replace(/\D+/g, '').length > 9 &&
-    Math.round(displayValue).toString().length < 9
-  ) {
-    const integerLength = Math.round(displayValue).toString().length
-    displayValue = displayValue?.toFixed(9 - integerLength)
-  }
-  // If the value has more than 9 digits, display it in scientific notation
-  else if (displayValue?.toString().replace(/\D+/g, '').length > 9) {
-    displayValue = displayValue.toExponential(0)
-  }
-  //If there is no value, output 0
-  else if (displayValue === null) {
-    displayValue = '0'
-  }
   return (
     <div className={`App ${theme}`}>
       <div className={`header`}>
         <span className={`title`}>calc</span>
         <ThemeSwitch theme={theme} setTheme={setTheme} className={`switch`} />
       </div>
-      <div className={`display`}>{displayValue}</div>
+      <div className={`display`}>{calcState[currentOperand]}</div>
       <div className={`operationPad`}>
         {operationButtons.map(({ value, action }, i) => (
           <Button
             key={`btn${i}`}
             onClick={() =>
-              dispatch({ value, type: action, payload: calcMemoInit })
+              onClickHandler({ value, currentOperand, type: action, payload: calcMemoInit })
             }
             value={value}
             i={i}
--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -1,14 +1,6 @@
 export function reducer(state, action) {
-  const { type, value, payload } = action
-  const { operator, operand1, operand2 } = state
-  const currOperand = operator && operand1 !== null ? 'operand2' : 'operand1'
-  const filledRequiredValues =
-    operand1 !== null && operand2 !== null && operator
-  const { calculated, floatBuffer, [currOperand]: currOperandVal } = state
-  const isNotValidValLength = (value) =>
-    value?.toString().replace(/\D+/g, '').length >= 9
-  const isInputNotAllowed =
-    isNotValidValLength(currOperandVal) || isNotValidValLength(floatBuffer)
+  const { type, value, currentOperand, payload } = action
+  const { operator, operand1, operand2, [currentOperand]: currOpValue } = state
 
   const isFloat = (n) => Number(n) === n && n % 1 !== 0
 
@@ -24,122 +16,16 @@ export function reducer(state, action) {
         return operand1 + operand2
     }
   }
-  function updateHandler() {
-    if (floatBuffer) {
-      return floatBufferHandler()
-    }
+  function updateHandler() {}
 
-    if (isFloat(currOperandVal) && value === 0) {
-      return {
-        ...state,
-        floatBuffer: `${currOperandVal}${value}`,
-        [currOperand]: null,
-      }
-    }
-
-    if (calculated) {
-      return {
-        ...state,
-        [currOperand]: value,
-        calculated: false,
-      }
-    }
-    return {
-      ...state,
-      [currOperand]:
-        currOperandVal !== null
-          ? parseFloat(`${state[currOperand]}${value}`)
-          : value,
-    }
-  }
-
-  function floatBufferHandler() {
-    const isFilledDecimal =
-      (floatBuffer.endsWith('.') || value !== 0 || floatBuffer.length <= 1) &&
-      !(floatBuffer.endsWith('.') && value === 0)
-    return {
-      ...state,
-      [currOperand]: parseFloat(floatBuffer + value),
-      floatBuffer: isFilledDecimal ? '' : floatBuffer + value,
-    }
-  }
-
+  console.log()
   switch (type) {
     case 'updateValue':
-      if (operator && operand1 === null && floatBuffer.endsWith('0')) {
-        return {
-          ...state,
-          [currOperand]: parseFloat(floatBuffer),
-          operand2: value,
-          floatBuffer: '',
-        }
-      }
-
-      if (operator && floatBuffer.endsWith('.') && currOperand !== 'operand2') {
-        return {
-          ...state,
-          operand1: Number(floatBuffer),
-          operand2: value,
-          floatBuffer: '',
-        }
-      }
-
-      if (operator && operand1 === null) {
-        return {
-          ...state,
-          operand1: 0,
-          operand2: value,
-        }
-      }
-
-      if (!isInputNotAllowed || calculated) {
-        return updateHandler()
-      }
-      return { ...state }
-
+      return { ...state, [currentOperand]: currOpValue + value }
     case 'setFloat':
-      if (calculated || isFloat(currOperandVal)) return { ...state }
-
-      if (!currOperandVal) {
-        return {
-          ...state,
-          floatBuffer: `0.`,
-          [currOperand]: null,
-        }
-      }
-
-      return {
-        ...state,
-        floatBuffer: currOperandVal + '.',
-        [currOperand]: null,
-      }
-
     case 'setOperator':
-      return { ...state, operator: value }
     case 'delDigit':
-      return { ...state, [currOperand]: null, floatBuffer: '' }
     case 'resetValues':
-      return { ...payload }
     case 'calcResult':
-      if (
-        (currOperand === 'operand2' && floatBuffer.endsWith('0')) ||
-        floatBuffer.endsWith('.')
-      ) {
-        return {
-          ...payload,
-          operand1: getResult({
-            ...state,
-            operand2: parseFloat(floatBuffer),
-          }),
-          calculated: true,
-        }
-      }
-      if (!filledRequiredValues) return { ...state }
-
-      return {
-        ...payload,
-        operand1: getResult(state),
-        calculated: true,
-      }
   }
 }
